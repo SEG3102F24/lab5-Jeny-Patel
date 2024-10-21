a6304035159a4254e45ff476f83fc5423d65f8bb
Jeny Patel <jenypatel2203@gmail.com>
2024-10-20 21:02:14 -0400

    Final commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+      apiKey: "AIzaSyDXKh3ulRo440LllkWU1dSEIx5mb0djC2U",
+      authDomain: "seglab5practice.firebaseapp.com",
+      projectId: "seglab5practice",
+      storageBucket: "seglab5practice.appspot.com",
+      messagingSenderId: "807901982093",
+      appId: "1:807901982093:web:1d188122e814c009f661c4",
+      measurementId: "G-J9HBDJTDHQ"
+  }
+};
